eb86fea327befa44674f7cab3a9ec3d2158fed2c
adshafqat <adshafqat@yahoo.com>
2023-11-05 11:37:26 +0000

    Update AppConfig.java

--- microservices/Bike-Service/src/main/java/com/redhat/Bike/Service/AppConfig.java
+++ microservices/Bike-Service/src/main/java/com/redhat/Bike/Service/AppConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.PropertySource;
+
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
